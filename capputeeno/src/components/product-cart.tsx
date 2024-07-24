70d3ace1604603ec3d2478e7574658970d88efa6
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-07-24 16:21:31 -0300

    feat: creating list of products

---
+++ capputeeno/src/components/product-cart.tsx
@@ -0,0 +1,62 @@
+import styled from "styled-components"
+
+interface ProductCard{
+
+    image: string 
+    title: string
+    price: number
+}
+
+
+const Card = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    
+    background-color: rgba(255, 255, 255, 0.4);
+    backdrop-filter: blur(10px);
+    border-radius: 0px 0px 4px 4px;
+
+    width: 256px;
+
+
+    img{
+        width: 256px;
+        height: 300px;
+    }
+
+    h3{
+        font-weight: 30px;
+        font-size: 16px;
+        line-height: 150%;
+        color: var(--text-dark2)
+
+    }
+
+    p{
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 150%;
+        color: var(--shapes-dark)
+    }
+
+    div{
+        width: 228px;
+        height: 1px;
+        top: 340px;
+        margin: 8px 0px;
+        background: var(--shapes);
+    }
+`
+
+export function ProductCard(props: ProductCard){
+    return (
+        <Card>   
+            <img src={props.image} />
+            <h3>{props.title}</h3>
+            <div></div>
+            <p>{props.price}</p>
+        </Card>
+    )  
+}
--- capputeeno/src/components/products-list.tsx
+++ capputeeno/src/components/products-list.tsx
@@ -1,15 +1,25 @@
 import { useProduct } from "@/hooks/useProducts"
+import { ProductCard } from "@/components/product-cart"
 
 interface ProductListProps{
 
 }
 
 export function ProductList(props: ProductListProps){
-    
+
     const { data } = useProduct();
     console.log(data)
 
     return (
-        <></>
+            <div>
+                {data?.map((product) => (
+                    <ProductCard
+                    key={product.id}
+                    image={product.image_url}
+                    title={product.name}
+                    price={product.price_in_cents}
+                    />
+                ))}
+            </div>
     )
 }
